79ea00f0d1187dfa54f1070d3a1e4f0f31120a43
Derkkon <113691157+Derkkon@users.noreply.github.com>
2023-02-25 21:38:28 +0100

    tabellen bij bestuur gezet

--- src/Pages/Bestuur.js
+++ src/Pages/Bestuur.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import TableBestuur from '../componenten/TableBestuur';
+import TableJeugdbestuur from '../componenten/TableJeugdbestuur';
 
 const Bestuur = () => {
     return (
@@ -6,49 +8,22 @@ const Bestuur = () => {
             <h1 className='text-3xl text-center p-5 font-bold text-red-600'>
                 Het Bestuur
             </h1> 
-            <h2 className='text-xl text-center mt-5 font-bold text-red-400 mb-5'>
+            <h2 className='text-xl text-center my-5 font-bold text-red-400'>
                 Jeugdbestuur & werking
             </h2>
             <div>
                 <h6 className='text-md text-center mt-5 font-regular mx-3'>
-                Naast het jeugdbestuur hebben we ook een jeugdwerking, dit zijn een gedreven groep vrijwilligers die zich
-                <br/> meer dan 100% inzetten voor onze jeugdwerking op verschillende vlakken. Denk maar aan:
-                <br/> Website, organiseren van events, sociale media , sponsoring, ontbijtmanden, afgevaardigden, tornooien,
-                <br/> familiedag, verkoop sleutelhangers, kledij,
-                stickerboek, pro soccer data, flames …
+                    Naast het jeugdbestuur hebben we ook een jeugdwerking, dit zijn een gedreven groep vrijwilligers die zich
+                    <br/> meer dan 100% inzetten voor onze jeugdwerking op verschillende vlakken. Denk maar aan:
+                    <br/> Website, organiseren van events, sociale media , sponsoring, ontbijtmanden, afgevaardigden, tornooien,
+                    <br/> familiedag, verkoop sleutelhangers, kledij,
+                    stickerboek, pro soccer data, flames …
                 </h6>
-                <div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Naam</th>
-                                <th>Functie</th>
-                                <th>E-mail</th>
-                                <th>Mobiel</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                                <td>Malcolm Lockyer</td>
-                                <td>1961</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Witchy Woman</td>
-                                <td>The Eagles</td>
-                                <td>1972</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Shining Star</td>
-                                <td>Earth, Wind, and Fire</td>
-                                <td>1975</td>
-                                <td></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                <TableJeugdbestuur/>
+                <h2 className='text-xl text-center my-5 font-bold text-red-400'>
+                    Bestuur bovenbouw
+                </h2>
+                <TableBestuur/>
             </div>
         </div>
     );
---
+++ src/componenten/TableBestuur.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const TableBestuur = () => {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Naam</th>
+                        <th>Functie</th>
+                        <th>E-mail</th>
+                        <th>Mobiel</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Everaerts Tim</td>
+                        <td>AVJO / Jeugdvoorzitter</td>
+                        <td>tim@scduffel.be</td>
+                        <td>0489/740210</td>
+                    </tr>
+                    <tr>
+                        <td>Bernaerts Peter</td>
+                        <td>OVJO</td>
+                        <td>peter@scduffel.be</td>
+                        <td>0486/686888</td>
+                    </tr>
+                    <tr>
+                        <td>Ruelens Gilles</td>
+                        <td>Secretaris</td>
+                        <td>gilles@scduffel.be</td>
+                        <td>0475/944088</td>
+                    </tr>
+                    <tr>
+                        <td>Vergaelen Paul</td>
+                        <td>Penningmeester</td>
+                        <td>paul@scduffel.be</td>
+                        <td>0478/887698</td>
+                    </tr>
+                    <tr>
+                        <td>Meulepas Ronny</td>
+                        <td>GC</td>
+                        <td>ronny@scduffel.be</td>
+                        <td>0474/635331</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default TableBestuur;
