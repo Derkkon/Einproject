43426e7960faed56543d6583bce173e99c05b8f2
Derkkon <siebe.vanpuyenbroeck@bazandpoort.be>
2023-02-27 14:16:45 +0100

    ScrollBanner aan gepast

--- src/componenten/ScrollBanner.js
+++ src/componenten/ScrollBanner.js
@@ -5,13 +5,21 @@ import sponor4 from '../assets/sponsors/nike_PNG20.png'
 
 
 const ScrollBanner = () => {
-    return(
+    return (
         <div class="sponsor-slider mt-10">
             <div class="flex flex-wrap sponsor-banner">
-                <img src={sponor1} className="w-1/12 md:w-40 grayscale hover:grayscale-0" alt={'Carrefour'}/>
-                <img src={sponor2} className="w-1/12 md:w-40 grayscale hover:grayscale-0" alt={'KBC'}/>
-                <img src={sponor3} className="w-1/12 md:w-40 grayscale hover:grayscale-0" alt={'Jako'}/>
-                <img src={sponor4} className="w-1/12 md:w-40 grayscale hover:grayscale-0" alt={'Nike'}/>
+                <a href='https://www.carrefour.be/nl.html' target={"_blank"} rel={"noreferrer"}>
+                    <img src={sponor1} className="w-1/12 md:w-40 grayscale hover:grayscale-0" alt={'Carrefour'} />
+                </a>
+                <a href='https://www.jako.be/nl/' target={"_blank"} rel={"noreferrer"}>
+                    <img src={sponor2} className="w-1/12 md:w-40 grayscale hover:grayscale-0" alt={'Jako'} />
+                </a>
+                <a href='https://www.kbc.be/particulieren/nl.html' target={"_blank"} rel={"noreferrer"}>
+                    <img src={sponor3} className="w-1/12 md:w-40 grayscale hover:grayscale-0" alt={'KBC'} />
+                </a>         
+                <a href='https://www.nike.com/be/' target={"_blank"} rel={"noreferrer"}>
+                    <img src={sponor4} className="w-1/12 md:w-40 grayscale hover:grayscale-0" alt={'Nike'} />
+                </a>
             </div>
         </div>
     )
