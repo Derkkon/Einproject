2d58d3f287412d6a896c8d6db94c265bf7b9598c
Derkkon <siebe.vanpuyenbroeck@bazandpoort.be>
2023-03-06 14:05:09 +0100

    gecentrrerd

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,58 +2,60 @@ import React from 'react';
 import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-  function sendEmail(e)  {
+  function sendEmail(e) {
     e.preventDefault();
 
     emailjs.sendForm('service_q8y8sj7', 'template_p2842qc', e.target, 'H0N5CF56xLZk5N-gI')
       .then((result) => {
-          console.log(result.text);
+        console.log(result.text);
       }, (error) => {
-          console.log(error.text);
+        console.log(error.text);
       });
     e.target.reset();
   };
 
 
   return (
-    <form className="w-full max-w-sm" onSubmit={sendEmail}>
-      <div className="md:flex md:items-center mb-6">
-        <div className="md:w-1/3">
-          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="user_name">
-            Naam
-          </label>
+    <div className='flex justify-center mt-10'>
+      <form className="w-full max-w-sm" onSubmit={sendEmail}>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="user_name">
+              Naam
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" name="user_name" />
+          </div>
         </div>
-        <div className="md:w-2/3">
-          <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="text" name="user_name" />
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="user_email">
+              E-mail
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="email" name="user_email" />
+          </div>
         </div>
-      </div>
-      <div className="md:flex md:items-center mb-6">
-        <div className="md:w-1/3">
-          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="user_email">
-            E-mail
-          </label>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="message">
+              Message
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <textarea className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" name="message" />
+          </div>
         </div>
-        <div className="md:w-2/3">
-          <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" type="email" name="user_email" />
-        </div>
-      </div>
-      <div className="md:flex md:items-center mb-6">
-        <div className="md:w-1/3">
-          <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="message">
-            Message
-          </label>
-        </div>
-        <div className="md:w-2/3">
-          <textarea className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" name="message" />
-        </div>
-      </div>
-      <div className="md:flex md:items-center">
-        <div className="md:w-1/3"></div>
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
           <div className="md:w-2/3">
             <input className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit" value="Send" />
           </div>
-      </div>
-    </form>
+        </div>
+      </form>
+    </div>
   );
 };
 
