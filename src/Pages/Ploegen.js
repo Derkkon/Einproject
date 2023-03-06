3a8d2e00fb9b7abe237c2e1a24c17b581ea32f8f
Derkkon <siebe.vanpuyenbroeck@bazandpoort.be>
2023-03-06 14:18:49 +0100

    inhoud verwijderd

--- src/Pages/Ploegen.js
+++ src/Pages/Ploegen.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import TableData from '../componenten/TableData';
-import { getAllAppData } from '../Data/PloegenData';
+
 
 const Ploegen = () => {
-    const ploeg = getAllAppData();
-    console.log(ploeg)
     return (
         <div>
-            <h1 className='text-3xl text-center p-5 font-bold text-red-600'>Ploegen</h1>
-            {ploeg.map(p =>
-                <TableData {...p}/>
-            )}
+            
         </div>
 
 
