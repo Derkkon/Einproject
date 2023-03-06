4fb08c5986e14ffee343c6a9a7c1ef7bd9996d68
Derkkon <113691157+Derkkon@users.noreply.github.com>
2023-03-06 09:31:32 +0100

    tabel activiteiten toegevoegd

--- src/Pages/Activiteiten.js
+++ src/Pages/Activiteiten.js
@@ -1,49 +1,11 @@
 import React from 'react';
+import TableActiviteiten from '../componenten/TableActiviteiten';
 
 const Activiteiten = () => {
     return (
-        <table className='table-fixed container mx-auto border-separate border border-slate-400'>
-            <thead>
-                <tr>
-                    <th className='border border-slate-300'>Naam</th>
-                    <th className='border border-slate-300'>Functie</th>
-                    <th className='border border-slate-300'>E-mail</th>
-                    <th className='border border-slate-300'>Mobiel</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td className='border border-slate-300'>Everaerts Tim</td>
-                    <td className='border border-slate-300'>AVJO / Jeugdvoorzitter</td>
-                    <td className='border border-slate-300'>tim@scduffel.be</td>
-                    <td className='border border-slate-300'>0489/740210</td>
-                </tr>
-                <tr>
-                    <td className='border border-slate-300'>fgh rgrh</td>
-                    <td className='border border-slate-300'>dgd</td>
-                    <td className='border border-slate-300'>ddgg</td>
-                    <td className='border border-slate-300'>gg</td>
-                </tr>
-                <tr>
-                    <td className='border border-slate-300'>dfsh th</td>
-                    <td className='border border-slate-300'>fdf</td>
-                    <td className='border border-slate-300'>fffd</td>
-                    <td className='border border-slate-300'>fdfff</td>
-                </tr>
-                <tr>
-                    <td className='border border-slate-300'>fdffbb dthg</td>
-                    <td className='border border-slate-300'>ffddf</td>
-                    <td className='border border-slate-300'>ff</td>
-                    <td className='border border-slate-300'>rhthdt</td>
-                </tr>
-                <tr>
-                    <td className='border border-slate-300'>rejuuj gjf</td>
-                    <td className='border border-slate-300'>dtht</td>
-                    <td className='border border-slate-300'>eht</td>
-                    <td className='border border-slate-300'>dht</td>
-                </tr>
-            </tbody>
-        </table>
+        <div className='my-5'>
+            <TableActiviteiten/>
+        </div>
     );
 };
 
---
+++ src/componenten/TableActiviteiten.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+const TableActiviteiten = () => {
+    return (
+        <table className='container mx-auto text-left border-separate border border-slate-400'>
+            <thead>
+                <tr>
+                    <th className='border border-slate-300'>Datum</th>
+                    <th className='border border-slate-300'>Activiteit</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td className='border border-slate-300'>Weekend week 15/8</td>
+                    <td className='border border-slate-300'>Familieweekend met mini tornooien (4 ploegen) alle leeftijden & barbecue op zaterdag</td>
+                </tr>
+                <tr>
+                    <td className='border border-slate-300'>1st weekend september</td>
+                    <td className='border border-slate-300'>Bedrukking kledij ten voordele van de jeugd</td>
+                </tr>
+                <tr>
+                    <td className='border border-slate-300'>1ste thuismatch 1ste ploeg</td>
+                    <td className='border border-slate-300'>Voorstelling jeugdploegen, alle ouders gratis ingang met na match penalty cup jeugd met keepers 1ste ploeg</td>
+                </tr>
+                <tr>
+                    <td className='border border-slate-300'>3de zaterdag september</td>
+                    <td className='border border-slate-300'>Deelname ballonnekesstoet in duffel</td>
+                </tr>
+                <tr>
+                    <td className='border border-slate-300'>OKtober</td>
+                    <td className='border border-slate-300'>Lasagne slag</td>
+                </tr>
+                <tr>
+                    <td className='border border-slate-300'>November</td>
+                    <td className='border border-slate-300'>Sinterklaasfeest met aansluitend 's avonds optredens voor de volwassenen on Cinema Plaza</td>
+                </tr>
+                <tr>
+                    <td className='border border-slate-300'>1ste week januari</td>
+                    <td className='border border-slate-300'>Receptie jeugdtrainers en vrijwilligers</td>
+                </tr>
+                <tr>
+                    <td className='border border-slate-300'>Januari - februari</td>
+                    <td className='border border-slate-300'>Stickerboek</td>
+                </tr>
+                <tr>
+                    <td className='border border-slate-300'>Laatste week januari</td>
+                    <td className='border border-slate-300'>Jeugdige kwis</td>
+                </tr>
+                <tr>
+                    <td className='border border-slate-300'>Weekend voor 14/2</td>
+                    <td className='border border-slate-300'>Valentijnsontbijt</td>
+                </tr>
+                <tr>
+                    <td className='border border-slate-300'>2de weekend mei</td>
+                    <td className='border border-slate-300'>Jeugdtornooi, vrijdag voortornooi barbecue ten voordele 1ste ploeg</td>
+                </tr>
+                <tr>
+                    <td className='border border-slate-300'>Laatste weekend mei</td>
+                    <td className='border border-slate-300'>Pasdag en informatiedag jeugd volgend seizoen</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+};
+
+export default TableActiviteiten;
